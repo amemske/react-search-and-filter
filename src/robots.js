98cce5b37969ef9b2e1530f1b2aa412eb65be504
amemske <olywave@gmail.com>
2020-08-27 15:32:32 +0300

    adding new  files

---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+{
+id: 1,
+name: 'James Doe',
+username: 'JD',
+email: 'jd@hello.com'
+},
+{
+id: 2,
+name: 'Edwin K',
+username: 'EK',
+email: 'ek@hello.com'
+},
+{
+id: 3,
+name: 'Mary Lewis',
+username: 'ML',
+email: 'ml@hello.com'
+},
+{
+id: 4,
+name: 'DeannaShaw',
+username: 'DS',
+email: 'ds@hello.com'
+},
+{
+id: 5,
+name: 'Matt Marks',
+username: 'MM',
+email: 'mm@hello.com'
+},
+{
+id: 6,
+name: 'Lee Madison',
+username: 'Ld',
+email: 'ld@hello.com'
+},
+{
+id: 7,
+name: 'Alice Wallner',
+username: 'AW',
+email: 'aw@hello.com'
+},
+{
+id: 8,
+name: 'Andrew Rome',
+username: 'AR',
+email: 'ar@hello.com'
+}
+
+
+];
